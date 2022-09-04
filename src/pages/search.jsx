8da2a2f311ab7e3b2a4cc28298cc74f22543c5ad
tom <t_hankinson@outlook.com>
2022-09-04 16:25:14 +0100

    Styled site look

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -3,6 +3,8 @@ import {React, useState} from "react";
 import { PokeResults } from "../content/pokeresults";
 
 import styled from "styled-components";
+import proff from "../assets/proffoak01.png"
+import pokeball from "../assets/pokeball.png"
 
 const { REACT_APP_POKE_KEY } = process.env
 
@@ -37,11 +39,29 @@ export const SearchPokemon =({pokemon, setPokemon})=>{
         <PokeSearchDiv>
             <SearchForm  onSubmit={SubmitHandler}>
             <input type="text" placeholder="Pokemon search" onChange={(e)=> setPokemon(e.target.value)}/>
-            <button>Search</button>
+            <PokeButton type="submit"><Pokeball src={pokeball} alt="pokeball"/></PokeButton>
             </SearchForm >
             
             <ReturnedResultsDiv>
-            {!chosen ? <p> please choose pokemon</p> : <PokeResults pokeResult={pokeResult}/>}
+            {!chosen ? 
+                        <InfoDiv>
+                            <Content>
+                                <h2>Welcome!</h2>
+                            <p>Hello there, I am Professor Oak</p>
+                            <p>This is a simple Pokedex</p>
+                            <p>Enter a name of a Pokemon in the above <br/> search bar to look it up </p>
+                            </Content>
+                            
+
+                            <ImageContainer>
+                                <IMG src={proff} alt="proff" />
+                            </ImageContainer>
+
+                           
+                        </InfoDiv> 
+                    : 
+                        <PokeResults pokeResult={pokeResult}/>
+            }
             </ReturnedResultsDiv>
         </PokeSearchDiv>
     )
@@ -68,6 +88,54 @@ const SearchForm = styled.form`
     border-bottom:  solid 2px #404040d1;
     background-color: #2a2a2a;
 `
+    const PokeButton = styled.button`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        background-color: transparent;
+   
+    `
+    const Pokeball = styled.img`
+        height: 40px;
+        &:hover{
+            height: 42px;
+            width: 42px;
+        }
+    `
+const InfoDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 90%;
+    height: 80%;
+    background-color: white;
+    border: solid 2px black;
+`
+    const Content = styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+        height: 100%;
+        
+    `
+    const ImageContainer = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+        height: 100%;
+        border-left: dashed 2px black ;
+    `
+    const IMG = styled.img`
+        width: 100px;
+        /* border: 1px solid blue; */
+    `
 const ReturnedResultsDiv = styled.div`
     display: flex;
     flex-direction: row;
